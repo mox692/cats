22d77ec9ad0d3bf1c5a7b1be8d70eb322832e030
mox692 <moymoymox@gmail.com>
2023-07-10 22:20:15 +0900

    fix unnecessary allocation

--- core/src/main/scala/cats/syntax/apply.scala
+++ core/src/main/scala/cats/syntax/apply.scala
@@ -22,15 +22,7 @@
 package cats
 package syntax
 
-trait ApplySyntax extends TupleSemigroupalSyntax {
-  implicit final def catsSyntaxApply[F[_], A](fa: F[A])(implicit F: Apply[F]): Apply.Ops[F, A] =
-    new Apply.Ops[F, A] {
-      type TypeClassType = Apply[F]
-
-      val self = fa
-      val typeClassInstance = F
-    }
-
+trait ApplySyntax extends TupleSemigroupalSyntax with Apply.ToApplyOps {
   implicit final def catsSyntaxApplyOps[F[_], A](fa: F[A]): ApplyOps[F, A] =
     new ApplyOps(fa)
 }
